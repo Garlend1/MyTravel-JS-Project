5f973b7969b2ba1525cf608f063cbd364ece12b3
Pavel <prmikhaylov@sberbank.ru>
2024-06-05 21:32:35 +0300

    backend requests added

--- src/components/PostItem/PostItem.jsx
+++ src/components/PostItem/PostItem.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 import './PostItem.css';
 import { Button } from 'antd';
 
-const PostItem = (props) => {
+const PostItem = ({post, onDelete }) => {
   return (
     <div className="post">
       <img
-        src={props.post.url}
-        alt={props.post.title}
+        src={post.url}
+        alt={post.title}
         className="post__image"
       />
       <div className="post__content">
         <strong>
-          {props.post.id}. {props.post.title}{' '}
+          {post.id}. {post.title}{' '}
         </strong>
-        <div>{props.post.body}</div>
+        <div>{post.body}</div>
       </div>
+
       <div className="post__btns">
-      <Button type="primary">Удалить</Button>
+        <Button type="primary" onClick={() => onDelete(post.id)}>
+          Удалить
+        </Button>
       </div>
     </div>
   );
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PostItem from '../PostItem/PostItem';
-import data from '../../data.json';
 import './PostList.css';
+import { BACKEND_URL } from '../../constants';
+import axios from 'axios';
 
-const PostList = ({ posts, title }) => {
+const PostList = () => {
+  const [posts, setPosts] = useState([])
+  const [error, setError] = useState(null)
+ 
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${BACKEND_URL}/api/posts`);
+        console.log(response.data.posts);
+        if (response.statusText !== 'OK') {
+          throw new Error('Не удалось получить данные с сервера');
+        }
+        setPosts(response.data.posts);
+        setError(null);
+      } catch (error) {
+        setError('Не удалось получить данные с сервера');
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+ 
+  const handleDeletePost =  async (postId) => {
+    try {
+      const response = await axios.delete(`${BACKEND_URL}/api/posts/${postId}`);
+      if (response.statusText !== 'No Content') {
+        throw new Error('Не удалось удалить пост');
+      }
+      setPosts(posts.filter((post) => post.id !== postId));
+      setError(null);
+    } catch (error) {
+      setError('Не удалось удалить пост');
+    }
+  };
+
+
   return (
-    <div className="post-list">
-      {data.posts.map((post) => (
-        <PostItem post={post} key={post.id} />
+    <div className="post-list-container">
+      {error && <div className='error-message'> Ошибка: {error}</div>}
+      <div className='post-list'>
+      {posts.map((post) => (
+        <PostItem post={post} key={post.id} onDelete = {handleDeletePost} />
       ))}
+    </div>
     </div>
   );
 };

d3ec6fe18e8fee3f4c3aea137f81f623b594b210
Pavel <prmikhaylov@sberbank.ru>
2024-06-10 21:52:53 +0300

    lesson 9 modal window

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,67 +1,15 @@
-// import React, { useEffect, useState } from 'react';
-// import PostItem from '../PostItem/PostItem';
-// import './PostList.css';
-// import { BACKEND_URL } from '../../constants';
-// import axios from 'axios';
-
-// const PostList = () => {
-//   const [posts, setPosts] = useState([])
-//   const [error, setError] = useState(null)
-
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get(`${BACKEND_URL}/api/posts`);
-//         console.log(response.data.posts);
-//         if (response.statusText !== 'OK') {
-//           throw new Error('Не удалось получить данные с сервера');
-//         }
-//         setPosts(response.data.posts);
-//         setError(null);
-//       } catch (error) {
-//         setError('Не удалось получить данные с сервера');
-//     }
-//   }
-
-//   useEffect(() => {
-//     fetchData()
-//   }, [])
-
-//   const handleDeletePost =  async (postId) => {
-//     try {
-//       const response = await axios.delete(`${BACKEND_URL}/api/posts/${postId}`);
-//       if (response.statusText !== 'No Content') {
-//         throw new Error('Не удалось удалить пост');
-//       }
-//       setPosts(posts.filter((post) => post.id !== postId));
-//       setError(null);
-//     } catch (error) {
-//       setError('Не удалось удалить пост');
-//     }
-//   };
-
-//   return (
-//     <div className="post-list-container">
-//       {error && <div className='error-message'> Ошибка: {error}</div>}
-//       <div className='post-list'>
-//       {posts.map((post) => (
-//         <PostItem post={post} key={post.id} onDelete = {handleDeletePost} />
-//       ))}
-//     </div>
-//     </div>
-//   );
-// };
-
-// export default PostList;
-
 import React, { useEffect, useState } from 'react';
-import { List, Card } from 'antd';
+import { List, Card, Button } from 'antd';
 import './PostList.css';
 import { BACKEND_URL } from '../../constants';
 import axios from 'axios';
 
+import CreatePostModal from '../PostModal/CreatePostModal';
+
 const PostList = () => {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   const fetchData = async () => {
     try {
@@ -80,6 +28,22 @@ const PostList = () => {
     fetchData();
   }, []);
 
+  const handleCreatePost = async (values) => {
+    console.log(values);
+    try {
+      const response = await axios.post(`${BACKEND_URL}/api/posts`, values);
+
+      if (response.data && response.data.id) {
+        setPosts([response.data, ...posts]);
+      } else {
+        throw new Error('Неправильный формат ответа API');
+      }
+      setError(null);
+    } catch (error) {
+      setError('Не удалось создать пост');
+    }
+  };
+
   const handleDeletePost = async (postId) => {
     try {
       const response = await axios.delete(`${BACKEND_URL}/api/posts/${postId}`);
@@ -95,6 +59,14 @@ const PostList = () => {
 
   return (
     <div className="post-list-container">
+      <Button type="primary" onClick={() => setIsModalVisible(true)}>
+        Создать пост
+      </Button>
+      <CreatePostModal
+        isModalVisible={isModalVisible}
+        setIsModalVisible={setIsModalVisible}
+        handleCreatePost={handleCreatePost}
+      />
       {error && <div className="error-message"> Ошибка: {error}</div>}
       <List
         grid={{
@@ -108,15 +80,15 @@ const PostList = () => {
         }}
         dataSource={posts}
         renderItem={(post, index) => (
-          <List.Item>
+          <List.Item key={index}>
             <Card
               cover={
                 post.url && (
-                  <img alt="example" src={post.url} className="post-image" />
+                  <img alt={post.title} src={post.url} className="post-image" />
                 )
               }
               key={post.id}
-              title={`${post.id}. ${post.title}}`}
+              title={`${post.id}. ${post.title}`}
               actions={[
                 <a key="delete" onClick={() => handleDeletePost(post.id)}>
                   Удалить
